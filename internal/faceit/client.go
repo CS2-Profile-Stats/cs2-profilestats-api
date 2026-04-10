0ce43d3e0e8b8768dd799567613a277656c26897
dom1torii <contact@domitori.xyz>
2026-04-10 15:52:18 +0300

    super_match_token isnt a subscription

--- internal/faceit/client.go
+++ internal/faceit/client.go
@@ -80,8 +80,11 @@ func (c *Client) GetProfile(ctx context.Context, game string, steamId string) (*
 	var membership *string
 	memberships, ok := playerData["memberships"].([]any)
 	if ok && len(memberships) > 0 {
-		v, _ := memberships[0].(string)
-		membership = &v
+    v, _ := memberships[0].(string)
+    if v == "super_match_token" {
+      v = "Free"
+    }
+    membership = &v
 	}
 
 	playerBans, err := c.fetchPlayerBans(ctx, *playerId)
