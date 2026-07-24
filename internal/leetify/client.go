ec02fb3eaf853a61a6f634c8aee91ce3d8af8f26
dom1torii <contact@domitori.xyz>
2026-07-24 20:39:05 +0300

    fix of a fix for leetify

--- internal/leetify/client.go
+++ internal/leetify/client.go
@@ -3,9 +3,13 @@ package leetify
 import (
 	"context"
 	"errors"
+	"net/http"
+
+	// "errors"
 	"fmt"
 	"math"
-	"net/http"
+
+	// "net/http"
 
 	"github.com/dom1torii/cs2-profilestats-api/internal/fetcher"
 	"github.com/dom1torii/cs2-profilestats-api/internal/utils"
@@ -48,99 +52,101 @@ func NewClient(apiKey string) *Client {
 }
 
 func (c *Client) GetProfile(ctx context.Context, steamId string) (*Profile, error) {
-	playerData, err := c.fetchPlayerData(ctx, steamId)
-	if err != nil {
-		return nil, fmt.Errorf("Failed fetching player data: %w", err)
-	}
+	playerData, pErr := c.fetchPlayerData(ctx, steamId)
 
-	name := utils.GetString(playerData, "name")
+	rawMatches, mErr := c.fetchPlayerMatches(ctx, steamId)
 
-	var leetifyRating *float64
+	var name *string
+
 	var premierRating *int
 	var wingmanRank *int
 	var competitiveRanks []CompetitiveRank
-	ranks, ok := playerData["ranks"].(map[string]any)
-	if ok {
-		if raw, ok := ranks["leetify"].(float64); ok {
-			v := math.Round(raw*100) / 100
-			leetifyRating = &v
-		}
-		premierRating = utils.GetInt(ranks, "premier")
-		wingmanRank = utils.GetInt(ranks, "wingman")
 
-		rawCompetitive, ok := ranks["competitive"].([]any)
-		if ok && len(rawCompetitive) > 0 {
-			competitiveRanks = make([]CompetitiveRank, 0, len(rawCompetitive))
-			for _, v := range rawCompetitive {
-				entry, ok := v.(map[string]any)
-				if !ok {
-					continue
-				}
-				competitiveRanks = append(competitiveRanks, CompetitiveRank{
-					Map:  utils.GetString(entry, "map_name"),
-					Rank: utils.GetInt(entry, "rank"),
-				})
-			}
-		}
-	}
+	var leetifyRating *float64
 
-	matches := utils.GetInt(playerData, "total_matches")
-	firstMatch := utils.GetString(playerData, "first_match_date")
+	var matches *int
+	var firstMatch *string
 
 	var winRate *int
-	if raw, ok := playerData["winrate"].(float64); ok {
-		v := int(math.Round(raw * 100))
-		winRate = &v
-	}
+	totalWins := 0
 
 	var aimRating, positioning, utility *int
 	var clutching, opening *float64
-	rating, ok := playerData["rating"].(map[string]any)
-	if ok {
-		aimRating = utils.GetInt(rating, "aim")
-		positioning = utils.GetInt(rating, "positioning")
-		utility = utils.GetInt(rating, "utility")
-
-		if raw, ok := rating["clutch"].(float64); ok {
-			v := math.Round(raw*100*100) / 100
-			clutching = &v
-		}
-		if raw, ok := rating["opening"].(float64); ok {
-			v := math.Round(raw*100*100) / 100
-			opening = &v
-		}
-	}
 
 	var preAim *float64
+	totalPreAim := 0.0
 	var reactionTime *int
-	statsData, ok := playerData["stats"].(map[string]any)
-	if ok {
-		if raw, ok := statsData["preaim"].(float64); ok {
-			v := math.Round(raw*100) / 100
-			preAim = &v
-		}
-		reactionTime = utils.GetInt(statsData, "reaction_time_ms")
-	}
+	totalReactionTime := 0
 
 	var kdRatio *float64
 	totalKdRatio := 0.0
 	matchCount := 0
-	rawMatches, err := c.fetchPlayerMatches(ctx, steamId)
-	if err != nil {
-		// if fetching matches fails (no idea why leetify doesn't return them for some profiles),
+
+	if pErr != nil || mErr != nil {
+		// if fetching playerData or matches fails (no idea why it happens for some profiles),
 		// we use leetify prod api (please dont sue me)
+
+		// combine errors, probably a bad solution, subject to change
+		combinedErr := errors.Join(pErr, mErr)
+
 		var apiErr *fetcher.APIError
-		if !errors.As(err, &apiErr) || apiErr.StatusCode != http.StatusNotFound {
-			return nil, fmt.Errorf("Failed fetching matches: %w", err)
+		if !errors.As(combinedErr, &apiErr) || apiErr.StatusCode != http.StatusNotFound {
+			return nil, fmt.Errorf("Failed fetching leetify data: %w", combinedErr)
 		}
 
-		fallbackMatches, fbErr := c.fetchPlayerMatchesFallback(ctx, steamId)
+		fallbackData, fbErr := c.fetchFallbackData(ctx, steamId)
 		if fbErr != nil {
 			return nil, fmt.Errorf("Failed fetching fallback for matches: %w", fbErr)
 		}
 
-		games, ok := fallbackMatches["games"].([]any)
+		recentRatings, ok := fallbackData["recentGameRatings"].(map[string]any)
 		if ok {
+			if raw, ok := recentRatings["leetify"].(float64); ok {
+				v := raw * 100
+				leetifyRating = &v
+			}
+
+			if raw, ok := recentRatings["aim"].(float64); ok {
+				v := int(math.Round(raw))
+				aimRating = &v
+			}
+
+			if raw, ok := recentRatings["positioning"].(float64); ok {
+				v := int(math.Round(raw))
+				positioning = &v
+			}
+
+			if raw, ok := recentRatings["utility"].(float64); ok {
+				v := int(math.Round(raw))
+				utility = &v
+			}
+
+			if raw, ok := recentRatings["clutch"].(float64); ok {
+				v := math.Round(raw*100*100) / 100
+				clutching = &v
+			}
+
+			if raw, ok := recentRatings["opening"].(float64); ok {
+				v := math.Round(raw*100*100) / 100
+				opening = &v
+			}
+		}
+
+		meta, ok := fallbackData["meta"].(map[string]any)
+		if ok {
+			name = utils.GetString(meta, "name")
+		}
+
+		games, ok := fallbackData["games"].([]any)
+		if ok {
+			gamesLen := len(games)
+			matches = &gamesLen
+
+			firstMatchData, ok := games[len(games)-1].(map[string]any)
+			if ok {
+				firstMatch = utils.GetString(firstMatchData, "gameFinishedAt")
+			}
+
 			// prod api returns ALL the matches, we limit to 100 because thats the amount public api returns
 			limit := min(len(games), 100)
 			for _, g := range games[:limit] {
@@ -148,16 +154,111 @@ func (c *Client) GetProfile(ctx context.Context, steamId string) (*Profile, erro
 				if !ok {
 					continue
 				}
+
 				kills := utils.GetFloat(game, "kills")
 				deaths := utils.GetFloat(game, "deaths")
 				if kills == nil || deaths == nil || *deaths == 0 {
 					continue
 				}
+
+				rawScores, ok := game["scores"].([]any)
+				if !ok || len(rawScores) < 2 {
+					continue
+				}
+				allyScore, ok0 := rawScores[0].(float64)
+				enemyScore, ok1 := rawScores[1].(float64)
+				if !ok0 || !ok1 {
+					continue
+				}
+				if allyScore > enemyScore {
+					totalWins++
+				}
+
+				if raw, ok := game["preaim"].(float64); ok {
+					v := math.Round(raw*100*100) / 100
+					totalPreAim += v
+				}
+
+				gameReactionTime := utils.GetInt(game, "reactionTime")
+				totalReactionTime += *gameReactionTime
+
 				totalKdRatio += *kills / *deaths
 				matchCount++
 			}
+
+			if matchCount > 0 {
+				rawWinRate := int(math.Round(float64(totalWins) / float64(matchCount) * 100))
+				winRate = &rawWinRate
+				// for some users preaim and reaction time are 0 for some reason, just dont set these in that case
+				if totalPreAim != 0 && totalReactionTime != 0 {
+					rawPreAim := math.Round(totalPreAim/float64(matchCount)*100) / 100
+					preAim = &rawPreAim
+					rawReactionTime := totalReactionTime / matchCount
+					reactionTime = &rawReactionTime
+				}
+			}
 		}
 	} else {
+		name = utils.GetString(playerData, "name")
+
+		ranks, ok := playerData["ranks"].(map[string]any)
+		if ok {
+			if raw, ok := ranks["leetify"].(float64); ok {
+				v := math.Round(raw*100) / 100
+				leetifyRating = &v
+			}
+			premierRating = utils.GetInt(ranks, "premier")
+			wingmanRank = utils.GetInt(ranks, "wingman")
+
+			rawCompetitive, ok := ranks["competitive"].([]any)
+			if ok && len(rawCompetitive) > 0 {
+				competitiveRanks = make([]CompetitiveRank, 0, len(rawCompetitive))
+				for _, v := range rawCompetitive {
+					entry, ok := v.(map[string]any)
+					if !ok {
+						continue
+					}
+					competitiveRanks = append(competitiveRanks, CompetitiveRank{
+						Map:  utils.GetString(entry, "map_name"),
+						Rank: utils.GetInt(entry, "rank"),
+					})
+				}
+			}
+		}
+
+		matches = utils.GetInt(playerData, "total_matches")
+		firstMatch = utils.GetString(playerData, "first_match_date")
+
+		if raw, ok := playerData["winrate"].(float64); ok {
+			v := int(math.Round(raw * 100))
+			winRate = &v
+		}
+
+		rating, ok := playerData["rating"].(map[string]any)
+		if ok {
+			aimRating = utils.GetInt(rating, "aim")
+			positioning = utils.GetInt(rating, "positioning")
+			utility = utils.GetInt(rating, "utility")
+
+			if raw, ok := rating["clutch"].(float64); ok {
+				v := math.Round(raw*100*100) / 100
+				clutching = &v
+			}
+			if raw, ok := rating["opening"].(float64); ok {
+				v := math.Round(raw*100*100) / 100
+				opening = &v
+			}
+		}
+
+		statsData, ok := playerData["stats"].(map[string]any)
+		if ok {
+			if raw, ok := statsData["preaim"].(float64); ok {
+				v := math.Round(raw*100) / 100
+				preAim = &v
+			}
+			reactionTime = utils.GetInt(statsData, "reaction_time_ms")
+		}
+
 		for _, m := range rawMatches {
 			match, ok := m.(map[string]any)
 			if !ok {
@@ -214,12 +315,12 @@ func (c *Client) fetchPlayerData(ctx context.Context, steamID string) (map[strin
 	return c.Fetch(ctx, url)
 }
 
-func (c *Client) fetchPlayerMatchesFallback(ctx context.Context, steamID string) (map[string]any, error) {
-	url := fmt.Sprintf("https://api.cs-prod.leetify.com/api/profile/id/%s", steamID)
-	return c.Fetch(ctx, url)
-}
-
 func (c *Client) fetchPlayerMatches(ctx context.Context, steamID string) ([]any, error) {
 	url := fmt.Sprintf("https://api-public.cs-prod.leetify.com/v3/profile/matches?steam64_id=%s", steamID)
 	return c.FetchArray(ctx, url)
+}
+
+func (c *Client) fetchFallbackData(ctx context.Context, steamID string) (map[string]any, error) {
+	url := fmt.Sprintf("https://api.cs-prod.leetify.com/api/profile/id/%s", steamID)
+	return c.Fetch(ctx, url)
 }
